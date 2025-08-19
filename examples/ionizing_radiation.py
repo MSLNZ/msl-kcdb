6e25fc593c2c74e28047c77fa6f169572c95687a
jborbely <joe.borbely@gmail.com>
2025-08-19 17:42:11 +1200

    rename "ir" variable to "radiation"

--- examples/ionizing_radiation.py
+++ examples/ionizing_radiation.py
@@ -2,23 +2,23 @@
 
 from msl.kcdb import Radiation
 
-ir = Radiation()
+radiation = Radiation()
 
 #
 # Get all reference data for the "Ionizing Radiation" metrology area
 #
-print(f"Getting all reference data for {ir.DOMAIN.name}...")
-metrology_areas = ir.metrology_areas()
+print(f"Getting all reference data for {radiation.DOMAIN.name}...")
+metrology_areas = radiation.metrology_areas()
 print(f"  There are {len(metrology_areas)} metrology areas")
-branches = [b for ma in metrology_areas for b in ir.branches(ma)]
+branches = [b for ma in metrology_areas for b in radiation.branches(ma)]
 print(f"  There are {len(branches)} branches")
-nuclides = ir.nuclides()
+nuclides = radiation.nuclides()
 print(f"  There are {len(nuclides)} nuclides")
-quantities = [q for b in branches for q in ir.quantities(b)]
+quantities = [q for b in branches for q in radiation.quantities(b)]
 print(f"  There are {len(quantities)} quantities")
-mediums = [m for b in branches for m in ir.mediums(b)]
+mediums = [m for b in branches for m in radiation.mediums(b)]
 print(f"  There are {len(mediums)} mediums")
-sources = [s for b in branches for s in ir.sources(b)]
+sources = [s for b in branches for s in radiation.sources(b)]
 print(f"  There are {len(sources)} sources")
 
 #
@@ -27,9 +27,9 @@ print(f"  There are {len(sources)} sources")
 # some information about each NMI
 #
 print("The following NMIs have capabilities to perform measurements with Beta radiation...")
-for source in ir.filter(sources, "Beta"):
+for source in radiation.filter(sources, "Beta"):
     # Here, we request the maximum number of elements that can be returned
     # by the KCDB server within a single request
-    results = ir.search(branch=source.branch, source=source, page_size=ir.MAX_PAGE_SIZE)
+    results = radiation.search(branch=source.branch, source=source, page_size=radiation.MAX_PAGE_SIZE)
     for data in results.data:
         print(f"  {data.nmi_code} ({data.instrument}): {data.radiation_specification}")
